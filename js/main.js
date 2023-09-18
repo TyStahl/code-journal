ec63f349fc9fe76e1f9837ba7712e925e18d0d20
Tyler Stahl <tstahl488@gmail.com>
2023-09-18 23:58:19 +0000

    started scripts

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const $imageUrl = document.querySelector('.image-url');
+const $img = document.querySelector('img');
+
+$imageUrl.addEventListener('click', setImage);
+
+function setImage(event) {
+  $img.setAttribute('src');
+}
