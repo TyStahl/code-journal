bb7435bc582b40d756fc762a531cd29b402eee8a
Tyler Stahl <tstahl488@gmail.com>
2023-09-20 17:38:21 +0000

    4.0 dom tree creation li in ul

--- js/main.js
+++ js/main.js
@@ -19,3 +19,39 @@ $entryForm.addEventListener('submit', function submitForm(event) {
   $img.setAttribute('src', './images/placeholder-image-square.jpg');
   $entryForm.reset();
 });
+
+function renderEntry(entry) {
+  const $unorderedList = document.createElement('ul');
+
+  const $listItem = document.createElement('li');
+  $listItem.setAttribute('class', 'row');
+  $unorderedList.appendChild($listItem);
+
+  const $columnHalf1 = document.createElement('div');
+  $columnHalf1.setAttribute('class', 'column-half');
+  $listItem.appendChild($columnHalf1);
+
+  const $img = document.createElement('img');
+  $img.setAttribute(
+    'src',
+    'https://archives.bulbagarden.net/media/upload/4/4a/0025Pikachu.png'
+  );
+  $columnHalf1.appendChild($img);
+
+  const $columnHalf2 = document.createElement('div');
+  $columnHalf2.setAttribute('class', 'column-half');
+
+  $listItem.appendChild($columnHalf2);
+
+  const $h3Title = document.createElement('h3');
+  $h3Title.textConent = 'Pikachu';
+  $columnHalf2.appendChild($h3Title);
+
+  const $pNotes = document.createElement('p');
+  $pNotes.textContent = 'Thunderbolt!';
+  $columnHalf2.appendChild($pNotes);
+
+  return $unorderedList;
+}
+
+renderEntry();
